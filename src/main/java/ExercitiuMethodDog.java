c96d5058ae6ec3053e8ef95a23b1c722b7d9b441
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-07-29 00:28:45 +0300

    am adaugat o tema pentru acasa

---
+++ src/main/java/ExercitiuMethodDog.java
@@ -0,0 +1,27 @@
+//Sa creati o clasa si in ea sa aveti 4 proprietati de tipuri diferite
+
+public class ExercitiuMethodDog {
+    String name;
+    int age;
+    double weight;
+    char firstletterofname;
+//metoda 1: Creati o metoda care returneaza si primeste ca parametru un nr intreg
+    public int height (int someint){
+        System.out.println(someint);
+        return someint;
+    }
+//metoda 2: Creati o metoda care primeste 2 parametri de tip string si nu returneaza nimic
+    public void dogfamily ( String s1, String s2){
+
+    }
+//metoda 3: Creati  o metoda care nu primeste nici un parametru si returneaza un string
+    public String nickname () {
+        ;
+        return "Tuzic";
+    }
+//metoda 4: Creati o metodata care primeste ca parametru un char si returneaza char-ul primit ca numar intreg.
+    public void  shortname (char somechar){
+        System.out.println(1);
+
+    }
+}
