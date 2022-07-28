3f9521c30b085b999d954164c8cc7110f647b90d
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-07-28 22:40:19 +0300

    am adaugat un fisier For

--- src/main/java/ExercitiuFor.java
+++ src/main/java/ExercitiuFor.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ExercitiuFor {
+/*Scrieti o clasa cu metoda main in ea. In main scrieti logica care calculeaza suma tuturor numerelor pina la un anumit numar.
+De ex.: Suma tuturor numerelor pina la 5 este 15.*/
+
+public class ExercitiuFor {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 4; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
 }
