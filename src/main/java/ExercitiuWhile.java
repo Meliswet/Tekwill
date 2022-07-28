502b0896527c6476846270335639cd3bfd17546e
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-07-28 22:41:24 +0300

    am adaugar un fisier While

--- src/main/java/ExercitiuWhile.java
+++ src/main/java/ExercitiuWhile.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ExercitiuWhile {
+/*Scrieti o clasa cu metoda main in ea. In main scrieti logica care calculeaza suma tuturor numerelor pina la un anumit numar.
+De ex.: Suma tuturor numerelor pina la 5 este 15.*/
+public class ExercitiuWhile {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= 4) {
+            sum += i++;
+        }
+        System.out.println(sum);
+    }
 }
