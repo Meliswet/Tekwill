0e1913758a745021b34dd13741012894e3e3b8b6
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-07-29 00:29:49 +0300

    am mai adaugat un exercitiu

--- src/main/java/ExercitiuMethodDogTest.java
+++ src/main/java/ExercitiuMethodDogTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ExercitiuMethodDogTest {
+public class ExercitiuMethodDogTest {
+    public static void main(String[] args){
+        ExercitiuMethodDog myDog = new ExercitiuMethodDog();
+      //metoda1 obiect
+       myDog.height( 5);
+
+       //metoda2 obiect
+       myDog.dogfamily("Sharik","Belka");
+
+        //metoda3 obiect
+        myDog.nickname();
+       String result = myDog.nickname();
+       System.out.println(result);
+
+        //metoda4 obiect
+       myDog.shortname( 'T');
+
+    }
 }
