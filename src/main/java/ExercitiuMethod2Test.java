2bffc93db361ee59f687b6ee4cdbeab9f9bf278a
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-07-29 17:46:24 +0300

    am adaugat obiectele la metode

--- src/main/java/ExercitiuMethod2Test.java
+++ src/main/java/ExercitiuMethod2Test.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ExercitiuMethod2Test {
+
+public class ExercitiuMethod2Test {
+
+    public static void main(String[] args){
+        ExercitiuMethod2 myExercitiu = new ExercitiuMethod2();
+        //Referinta Metoda1
+        myExercitiu.countSum( 6);
+
+        //Referinta Metoda2
+        myExercitiu.showEvenNumbers( 6);
+
+        //Referinta Metoda3
+        myExercitiu.say( "Hello");
+
+        ////Referinta Metoda4
+        myExercitiu.showOddNumbers( 6);
+
+    }
 }
