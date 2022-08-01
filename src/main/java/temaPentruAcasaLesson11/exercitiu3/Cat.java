5dce082e773b4fe2313f51671b0c0b23deb0a992
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-08-01 14:15:37 +0300

    am adaugat exercitiu3

--- src/main/java/temaPentruAcasaLesson11/exercitiu3/Cat.java
+++ src/main/java/temaPentruAcasaLesson11/exercitiu3/Cat.java
@@ -1,2 +1,19 @@
-package temaPentruAcasaLesson11.exercitiu3;public class Cat {
+
+/*Construiti o clasa si in ea sa veti 2 proprietati private. Gasiti un mod de a seta si citi valoarea
+proprietatilor din afara fara a schimba alt modificator de acces.*/
+package temaPentruAcasaLesson11.exercitiu3;
+
+public class Cat {
+    private String name;
+    private int age;
+
+    public String getName() {
+        System.out.println ("Murzic");
+        return name;
+    }
+
+    public int getAge() {
+        System.out.println(3);
+        return age;
+    }
 }
--- src/main/java/temaPentruAcasaLesson11/exercitiu3/TestCat.java
+++ src/main/java/temaPentruAcasaLesson11/exercitiu3/TestCat.java
@@ -1,2 +1,11 @@
-package temaPentruAcasaLesson11.exercitiu3;public class TestCat {
+/*Construiti o clasa si in ea sa veti 2 proprietati private. Gasiti un mod de a seta si citi valoarea
+proprietatilor din afara fara a schimba alt modificator de acces.*/
+package temaPentruAcasaLesson11.exercitiu3;
+
+public class TestCat {
+    public static void main (String []args) {
+        Cat myCat = new Cat();
+        myCat.getName();
+        myCat.getAge();
+    }
 }
