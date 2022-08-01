8af19ab5449115708ecf65fe05dc26d25649cee0
Mihaela Pilipenco <catrinescu_mihaela@mail.ru>
2022-08-01 14:15:19 +0300

    am adaugat exercitiu 2

--- src/main/java/temaPentruAcasaLesson11/exercitiu2/BB.java
+++ src/main/java/temaPentruAcasaLesson11/exercitiu2/BB.java
@@ -1,2 +1,12 @@
-package temaPentruAcasaLesson11.exercitiu2;public class BB {
+/* Construiti o clasa si in ea sa aveti o metoda publica care cheama in ea alte 2 metode private.
+Apoi din afara pachetului accesati metoda publica.*/
+package temaPentruAcasaLesson11.exercitiu2;
+import temaPentruAcasaLesson11.exercitiu2.pack1.AA;
+
+public class BB {
+
+    public static void main(String[] args){
+        AA aa=new AA();
+        aa.metodaDeBaza();
+    }
 }
--- src/main/java/temaPentruAcasaLesson11/exercitiu2/pack1/AA.java
+++ src/main/java/temaPentruAcasaLesson11/exercitiu2/pack1/AA.java
@@ -1,2 +1,20 @@
-package temaPentruAcasaLesson11.exercitiu2.pack1;public class AA {
+/* Construiti o clasa si in ea sa aveti o metoda publica care cheama in ea alte 2 metode private.
+Apoi din afara pachetului accesati metoda publica.*/
+package temaPentruAcasaLesson11.exercitiu2.pack1;
+
+public class AA {
+
+    public void metodaDeBaza(){
+        metodaAjutatoare();
+        metodaAjutatoare1();
+
+    }
+
+    private void metodaAjutatoare(){
+        System.out.println("Cat");
+    }
+    private void metodaAjutatoare1(){
+        System.out.println("Dog");
+    }
+
 }
